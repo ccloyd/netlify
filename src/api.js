e54f451407deaabb90c7017b39dd9611dabe6fdf
Asrarul Ikram <eternanymous.gmail.com>
2022-06-21 02:04:49 +0800

    add: array masjid, route all masjid & masjid by id

--- src/api.js
+++ src/api.js
@@ -1,262 +1,28 @@
-const express = require('express');
-const serverless = require('serverless-http');
+const express = require("express");
+const serverless = require("serverless-http");
+const { masjid } = require("./data/masjid");
+const { ustadz } = require("./data/ustadz");
 
 const app = express();
 const router = express.Router();
 
-router.get("/", (req,res) => {
- res.json(
-   [
-      {
-        "id": 1,
-        "name": "Masjid Luqmanul Hakim",
-        "address": {
-          "street": "Jl. Waduk Tunggu Pampang",
-          "geo": {
-            "lat": "-5.166060",
-            "lng": "119.474049"
-          }
-        },
-        "phone": "081347730863 (Abu Fadhil)",
-        "picture": "-",
-      },
-      {
-        "id": 2,
-        "name": "Masjid Al Ikhlas Pannampu",
-        "address": {
-          "street": "Jl. Tinumbu lorong.165 c Belakang Pasar Pannampu",
-          "geo": {
-            "lat": "-5.115540",
-            "lng": "119.425119"
-          }
-        },
-        "phone": "-",
-        "picture": "-",
-      },
-      {
-        "id": 3,
-        "name": "Masjid Agung 45",
-        "address": {
-          "street": "-",
-          "geo": {
-            "lat": "-5.139160",
-            "lng": "119.444969"
-          }
-        },
-        "phone": "-",
-        "picture": "-",
-      },
-      {
-        "id": 4,
-        "name": "Masjid Khadijah Sudiang",
-        "address": {
-          "street": "",
-          "geo": {
-            "lat": "-5.084417",
-            "lng": "119.530343"
-          }
-        },
-        "phone": "085205050999",
-        "picture": "-",
-      },
-      {
-        "id": 5,
-        "name": "Masjid Ar-Risalah",
-        "address": {
-          "street": "Kantor Regional X PT. Pos Indonesia, Jl. Pettarani (Gedung Orange Samping Bulog)",
-          "geo": {
-            "lat": "-5.164621",
-            "lng": "119.434613"
-          }
-        },
-        "phone": "-",
-        "picture": "-",
-      },
-      {
-        "id": 6,
-        "name": "Masjid Nurul Bahri",
-        "address": {
-          "street": "Jl. Galangan Kapal",
-          "geo": {
-            "lat": "-5.115801",
-            "lng": "119.430449"
-          }
-        },
-        "phone": "-",
-        "picture": "-",
-      },
-      {
-        "id": 7,
-        "name": "Mushollah Fakultas Kedokteran dan Ilmu Kedokteran Kampus II Uin Alauddin Samata",
-        "address": {
-          "street": "-",
-          "geo": {
-            "lat": "-5.205107",
-            "lng": "119.499336"
-          }
-        },
-        "phone": "085299956345 (Ikhsan), 08135500156 (Pak Rusdi)",
-        "picture": "-",
-      },
-      {
-        "id": 8,
-        "name": "Mushollah Trans Studio Mall",
-        "address": {
-          "street": "Jl. Waduk Tunggu Pampang",
-          "geo": {
-            "lat": "-5.160338",
-            "lng": "119.394276"
-          }
-        },
-        "phone": "082347770632 (Hasrullah),085395455156 (Supriadi)",
-        "picture": "-",
-      },
-      {
-        "id": 9,
-        "name": "Masjid Nurul Falah Pari'risi",
-        "address": {
-          "street": "-",
-          "geo": {
-            "lat": "-5.432430",
-            "lng": "119.437243"
-          }
-        },
-        "phone": "082349561546",
-        "picture": "-",
-      },
-      {
-        "id": 10,
-        "name": "Masjid Baitul",
-        "address": {
-          "street": "Jl. Lasinrang",
-          "geo": {
-            "lat": "-5.145936",
-            "lng": "119.413949"
-          }
-        },
-        "phone": "082394741414",
-        "picture": "-",
-      },
-      {
-        "id": 11,
-        "name": "Masjid Graha Jannah",
-        "address": {
-          "street": " Jl. Sultan Alauddin. Komp. Perumahan Graha Modern Jaya, Depan Brimob",
-          "geo": {
-            "lat": "-5.171103",
-            "lng": "119.423916"
-          }
-        },
-        "phone": "08991887777",
-        "picture": "-",
-      },
-      {
-        "id": 12,
-        "name": "Mushollah Royal Apartemen (Belakang Ramayana Pettarani)",
-        "address": {
-          "street": "-",
-          "geo": {
-            "lat": "-5.153322",
-            "lng": "119.438928"
-          }
-        },
-        "phone": "-",
-        "picture": "-",
-      },
-      {
-        "id": 13,
-        "name": "Masjid Baiturrahim",
-        "address": {
-          "street": "Jl. Gontang Selatan",
-          "geo": {
-            "lat": "-5.185778",
-            "lng": "119.400505"
-          }
-        },
-        "phone": "089687156736, 085824821741",
-        "picture": "-",
-      },
-      {
-        "id": 14,
-        "name": "Masjid Daarul Falah",
-        "address": {
-          "street": "Jl. Pampang 4 No. 150, Belakang Rusunawa UMI",
-          "geo": {
-            "lat": "-5.134661",
-            "lng": "119.453543"
-          }
-        },
-        "phone": "082190655492 (Akh Muhammad), 082193188713 (Abu Waqi)",
-        "picture": "-",
-      },
-      {
-        "id": 15,
-        "name": "Masjid Babus Salam",
-        "address": {
-          "street": "Jl. Cakalang III",
-          "geo": {
-            "lat": "-5.116604",
-            "lng": "119.420064"
-          }
-        },
-        "phone": "082349981945 (Tri Abu Zakariyyah), 082344666103 (Akbar)",
-        "picture": "-",
-      },
-      {
-        "id": 16,
-        "name": "Mushollah Al Haudh Ratulangi",
-        "address": {
-          "street": "Jl. Ratulangi No.72, Samping Toko Agung",
-          "geo": {
-            "lat": "-5.154109",
-            "lng": "119.416005"
-          }
-        },
-        "phone": "082199116067",
-        "picture": "-",
-      },
-      {
-        "id": 17,
-        "name": "Masjid Nurul Iman",
-        "address": {
-          "street": "Jl. Batua Raya No.129",
-          "geo": {
-            "lat": "-5.156510",
-            "lng": "119.458428"
-          }
-        },
-        "phone": "081342351283 (Akhiruddin), 081355929448 (Syahrul)",
-        "picture": "-",
-      },
-      {
-        "id": 18,
-        "name": "Masjid Darur Rahmah",
-        "address": {
-          "street": "Jl. Satangnga, Kompleks Pasar Sentral",
-          "geo": {
-            "lat": "-5.128010",
-            "lng": "119.416642"
-          }
-        },
-        "phone": "085211627969",
-        "picture": "-",
-      },
-      {
-        "id": 19,
-        "name": "Masjid Jannatul Firdaus",
-        "address": {
-          "street": "BTP Blok H, Samping Indomaret",
-          "geo": {
-            "lat": "-5.141654",
-            "lng": "119.509886"
-          }
-        },
-        "phone": "",
-        "picture": "-",
-      },
-    ]);
+router.get("/ustadz", (req, res) => {
+  res.json(ustadz);
 });
 
-app.use('/.netlify/functions/api', router);
+router.get("/masjid", (req, res) => {
+  res.json(masjid);
+});
+
+router.get("/masjid/:id", (req, res) => {
+  const data = masjid.find((x) => x.id === parseInt(req.params.id));
+  res.json(data);
+});
+
+router.get("/", (req, res) => {
+  res.json({ welcome: "Ta'lim Finder" });
+});
+
+app.use("/.netlify/functions/api", router);
 
 module.exports.handler = serverless(app);
