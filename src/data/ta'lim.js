6f2127351a0a310a6b870d1e19e6f7b86f61c2d6
ccloyd <fadhilsuzaku@gmail.com>
2022-06-30 17:18:51 +0800

    update lagi

---
+++ src/data/ta'lim.js
@@ -0,0 +1,374 @@
+export const masjid = [
+  {
+    id: 1,
+    name: "Kitab Minhajus Salikin wa Taudhihul Fiqhi fid Diin",
+    data_tempat: {
+        id: "1",
+		name: "Masjid Luqmanul Hakim"},
+    waktu: "18:30",
+    data_ustadz: {
+        id: "9",
+        name: "Ustadz Ruwaifi Rahmat"},
+    link: "-",
+  },
+  {
+    id: 2,
+    name: "Kitab Tauhid",
+    data_tempat: {
+        id: "1",
+		name: "Masjid Luqmanul Hakim"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "1",
+		name: "Ustadz Abdul Mu'thi"},
+	link: "-",
+  },
+  {
+    id: 3,
+    name: "Kitab Syahrussunnah",
+    data_tempat: {
+        id: "2",
+		name: "Masjid Al Ikhlas Pannampu"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "2",
+		name: "Ustadz Ahmad Alfian"},
+	link: "-",
+  },
+  {
+    id: 4,
+    name: "Syarah Al Arba'in An Nawawiah",
+    data_tempat: {
+        id: "3",
+		name: "Masjid Agung 45"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "7",
+		name: "Ustadz Muhammad Rijal"},
+	link: "-",
+  },
+  {
+    id: 5,
+    name: "Mengenal Nama-Nama dan Sifat-Sifat Allah",
+    data_tempat: {
+        id: "4",
+		name: "Masjid Khadijah Sudiang"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "12",
+		name: "Ustadz Abu Abdirrahman Thariq"},
+	link: "-",
+  },
+  {
+    id: 6,
+    name: "Kitab Tauhid As Shogir",
+    data_tempat: {
+        id: "5",
+		name: "Masjid Ar-Risalah"},
+    waktu: "16:00",
+	data_ustadz: {
+        id: "6",
+		name: "Ustadz Harits Marekar"},
+	link: "-",
+  },
+  {
+    id: 7,
+    name: "Kitab Ad Daa wa Da Waa",
+    data_tempat: {
+        id: "6",
+		name: "Masjid Nurul Bahri"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "2",
+		name: "Ustadz Ahmad Alfian"},
+	link: "-",
+  },
+  {
+    id: 8,
+    name: "Syarah Qasidah Al Imam Al Jurjani",
+    data_tempat: {
+        id: "7",
+		name: "Mushollah Fakultas Kedokteran dan Ilmu Kedokteran Kampus II Uin Alauddin Samata"},
+    waktu: "16:00",
+	data_ustadz: {
+        id: "5",
+		name: "Ustadz Ashari Asri"},
+	link: "-",
+  },
+  {
+    id: 9,
+    name: "Kitab Umdatul Fiqh",
+    data_tempat: {
+        id: "5",
+		name: "Masjid Ar-Risalah"},
+    waktu: "12:30",
+	data_ustadz: {
+        id: "10",
+		name: "Ustadz Sa'ad Baisa"},
+	link: "-",
+  },
+  {
+    id: 10,
+    name: "Sifat Sholat Nabi",
+    data_tempat: {
+        id: "8",
+		name: "Mushollah Trans Studio Mall"},
+    waktu: "12:30",
+	data_ustadz: {
+        id: "12",
+		name: "Ustadz Abu Abdirrahman Thariq"},
+	link: "-",
+  },
+  {
+    id: 11,
+    name: "Riyadhus Sholihin",
+    data_tempat: {
+        id: "9",
+		name: "Masjid Nurul Falah Pari'risi"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "10",
+		name: "Ustadz Sa'ad Baisa"},
+	link: "-",
+  },
+  {
+    id: 12,
+    name: "Matan Abu Syuja'",
+    data_tempat: {
+        id: "2",
+		name: "Masjid Al Ikhlas Pannampu"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "1",
+		name: "Ustadz Abdul Mu'thi"},
+	link: "-",
+  },
+  {
+    id: 13,
+    name: "Fathul Majid",
+    data_tempat: {
+        id: "10",
+		name: "Masjid Baitul"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "5",
+		name: "Ustadz Ashari Asri"},
+	link: "-",
+  },
+  {
+    id: 14,
+    name: "Adab Menyikapi Perselisihan",
+    data_tempat: {
+        id: "4",
+		name: "Masjid Khadijah Sudiang"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "9",
+		name: "Ustadz Ruwaifi Rahmat"},
+	link: "-",
+  },
+  {
+    id: 15,
+    name: "Pembahasan Sholat",
+    data_tempat: {
+        id: "11",
+		name: "Masjid Graha Jannah"},
+    waktu: "16:00",
+	data_ustadz: {
+        id: "11",
+		name: "Ustadz Sarbini"},
+	link: "-",
+  },
+  {
+    id: 16,
+    name: "Kitabul Jami'",
+    data_tempat: {
+        id: "5",
+		name: "Masjid Ar-Risalah"},
+    waktu: "16:00",
+	data_ustadz: {
+        id: "1",
+		name: "Ustadz Abdul Mu'thi"},
+	link: "-",
+  },
+  {
+    id: 17,
+    name: "Al Aqidah Al Wasithiyah",
+    data_tempat: {
+        id: "12",
+		name: "Mushollah Royal Apartemen (Belakang Ramayana Pettarani)"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "12",
+		name: "Ustadz Abu Abdirrahman Thariq"},
+	link: "-",
+  },
+  {
+    id: 18,
+    name: "Fiqih Ibadah",
+    data_tempat: {
+        id: "13",
+		name: "Masjid Baiturrahim"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "5",
+		name: "Ustadz Ashari Asri"},
+	link: "-",
+  },
+  {
+    id: 19,
+    name: "Mukhtashor Minhajil Qoshidin",
+    data_tempat: {
+        id: "1",
+		name: "Masjid Luqmanul Hakim"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "4",
+		name: "Ustadz Ammar"},
+	link: "-",
+  },
+  {
+    id: 20,
+    name: "50 Hadits Pondasi Islam",
+    data_tempat: {
+        id: "12",
+		name: "Mushollah Royal Apartemen (Belakang Ramayana Pettarani)"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "9",
+		name: "Ustadz Ruwaifi Rahmat"},
+	link: "-",
+  },
+  {
+    id: 21,
+    name: "Kitab Ahkam Al Udhiyah wa Adz Dzakah",
+    data_tempat: {
+        id: "14",
+		name: "Masjid Daarul Falah"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "9",
+		name: "Ustadz Ruwaifi Rahmat"},
+	link: "-",
+  },
+  {
+    id: 22,
+    name: "Syarh Manzhumah Mimiyah",
+    data_tempat: {
+        id: "15",
+		name: "Masjid Babus Salam"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "8",
+		name: "Ustadz Muhammad Umar As-Sewed"},
+	link: "-",
+  },
+  {
+    id: 23,
+    name: "Fiqih Ibadah",
+    data_tempat: {
+        id: "4",
+		name: "Masjid Khadijah Sudiang"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "3",
+		name: "Ustadz Ahmad Khodim"},
+	link: "-",
+  },
+  {
+    id: 24,
+    name: "Al Aqidah Al Wasithiyah",
+    data_tempat: {
+        id: "1",
+		name: "Masjid Luqmanul Hakim"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "3",
+		name: "Ustadz Ahmad Khodim"},
+	link: "-",
+  },
+  {
+    id: 25,
+    name: "Ttsalatsatul Ushul",
+    data_tempat: {
+        id: "16",
+		name: "Mushollah Al Haudh Ratulangi"},
+    waktu: "16:00",
+	data_ustadz: {
+        id: "8",
+		name: "Ustadz Muhammad Umar As-Sewed"},
+	link: "-",
+  },
+  {
+    id: 26,
+    name: "Ttsalatsatul Ushul",
+    data_tempat: {
+        id: "17",
+		name: "Masjid Nurul Iman"},
+    waktu: "16:00",
+	data_ustadz: {
+        id: "7",
+		name: "Ustadz Muhammad Rijal"},
+	link: "-",
+  },
+  {
+    id: 27,
+    name: "Shahih Fiqh As Sunnah",
+    data_tempat: {
+        id: "18",
+		name: "Masjid Darur Rahmah"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "10",
+		name: "Ustadz Sa'ad Baisa"},
+	link: "-",
+  },
+  {
+    id: 28,
+    name: "Ushulus Sunnah",
+    data_tempat: {
+        id: "19",
+		name: "Masjid Jannatul Firdaus"},
+    waktu: "09:30",
+	data_ustadz: {
+        id: "7",
+		name: "Ustadz Muhammad Rijal"},
+	link: "-",
+  },
+  {
+    id: 29,
+    name: "Fiqih Thaharah",
+    data_tempat: {
+        id: "19",
+		name: "Masjid Jannatul Firdaus"},
+    waktu: "11:00",
+	data_ustadz: {
+        id: "12",
+		name: "Ustadz Abu Abdirrahman Thariq"},
+	link: "-",
+  },
+  {
+    id: 30,
+    name: "Tafsir dan Hadits Pilihan/Tematik",
+    data_tempat: {
+        id: "4",
+		name: "Masjid Khadijah Sudiang"},
+    waktu: "09:00",
+	data_ustadz: {
+        id: "9",
+		name: "Ustadz Ruwaifi Rahmat"},
+	link: "-",
+  },
+  {
+    id: 31,
+    name: "Kajian Aqidah",
+    data_tempat: {
+        id: "4",
+		name: "Masjid Khadijah Sudiang"},
+    waktu: "18:30",
+	data_ustadz: {
+        id: "5",
+		name: "Ustadz Ashari Asri"},
+	link: "-",
+  },
+]
--- src/data/ustadz.js
+++ src/data/ustadz.js
@@ -1,1 +1,74 @@
-export const ustadz = [];
+export const ustadz = [
+  {
+    id: 1,
+    name: "Ustadz Abdul Mu'thi",
+    phone: "+62 812-5422-4146",
+    picture: "-",
+  },
+  {
+    id: 2,
+    name: "Ustadz Ahmad Alfian",
+    phone: "+62 852-3652-3991",
+    picture: "-",
+  },
+  {
+    id: 3,
+    name: "Ustadz Ahmad Khodim",
+    phone: "+62 812-3365-2766",
+    picture: "-",
+  },
+  {
+    id: 4,
+    name: "Ustadz Ammar",
+    phone: "+62 823-1969-8093",
+    picture: "-",
+  },
+  {
+    id: 5,
+    name: "Ustadz Ashari Asri",
+    phone: "+62 813-4252-3635",
+    picture: "-",
+  },
+  {
+    id: 6,
+    name: "Ustadz Harits Marekar",
+    phone: "+62 812-3136-3631",
+    picture: "-",
+  },
+  {
+    id: 7,
+    name: "Ustadz Muhammad Rijal",
+    phone: "+62 852-5582-8938",
+    picture: "-",
+  },
+  {
+    id: 8,
+    name: "Ustadz Muhammad Umar As-Sewed",
+    phone: "+62 821-2727-0002",
+    picture: "-",
+  },
+  {
+    id: 9,
+    name: "Ustadz Ruwaifi Rahmat",
+    phone: "+62 812-8111-5434",
+    picture: "-",
+  },
+  {
+    id: 10,
+    name: "Ustadz Sa'ad Baisa",
+    phone: "+62 852-4807-6959",
+    picture: "-",
+  },
+  {
+    id: 11,
+    name: "Ustadz Sarbini",
+    phone: "+62 852-9206-4041",
+    picture: "-",
+  },
+  {
+    id: 12,
+    name: "Ustadz Abu Abdirrahman Thariq",
+    phone: "+62 813-4380-8951",
+    picture: "-",
+  },
+];
